ae30967047cd52a5aac3814545fabb521870abfd
Youri ANTENOR-HABAZAC <youri.ah.freelance@gmail.com>
2022-02-20 23:53:27 +0100

    Add some last optimization

--- data/src/main/java/com/example/data/GsonUtils.kt
+++ data/src/main/java/com/example/data/GsonUtils.kt
@@ -5,8 +5,6 @@ import com.google.gson.GsonBuilder
 import java.util.Date
 
 object GsonUtils {
-
     val gson: Gson = GsonBuilder()
-        //.registerTypeAdapter(JsonItemType::class.java, JsonItemTypeDeserializer)
         .create()
 }
